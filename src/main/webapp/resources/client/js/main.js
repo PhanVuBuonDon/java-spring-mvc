c646c4bf07d2d2aa31633bc93a730b5d3b33cecc
Phan Vu <phanvubuondon@gmail.com>
2025-01-20 19:37:20 +0700

    #156. Add Sort

--- src/main/webapp/resources/client/js/main.js
+++ src/main/webapp/resources/client/js/main.js
@@ -244,6 +244,11 @@
         // Add or update query parameters
         searchParams.set('page', '1');
         searchParams.set('sort', sortValue);
+
+        searchParams.delete('factory');
+        searchParams.delete('target');
+        searchParams.delete('price');
+
         if (factoryArr.length > 0) {
             searchParams.set('factory', factoryArr.join(','));
         }
